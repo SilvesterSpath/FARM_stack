deeca80263b08e9dd8c8b1c3a2710ce1086ce736
SilvesterSpath <spath.sz@gmail.com>
2024-06-11 12:30:13 +0200

    basic routes

--- backend/main.py
+++ backend/main.py
@@ -23,3 +23,22 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/api/todo")
+async def get_todo():
+    return {"todo": "todo"}
+
+@app.get("/api/todo/{id}")
+async def get_todo_by_id(id):
+    return {"todo": id}
+
+@app.post("/api/todo")
+async def post_todo(todo):
+    return {"todo": todo}
+
+@app.put("/api/todo/{id}")
+async def put_todo(id, data):
+    return {"todo": id, "data": data}
+
+@app.delete("/api/todo/{id}")
+async def delete_todo(id):
+    return {"todo": id}
