77aa72ef3ecfc9d685578f1e16042ff6e5f1e50d
SilvesterSpath <spath.sz@gmail.com>
2024-06-12 09:02:21 +0200

    frontend UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([{}]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
     <div className='App'>
-      <h1>Welcome to the Farm Stack</h1>
       <div
         className='App list-group-item justify-content-center align-items-center mx-auto'
         style={{
           width: '400px',
-          backgroundColor: 'gray',
+          backgroundColor: 'white',
           marginTop: '15px',
+          padding: '10px',
+          borderRadius: '3px',
         }}
       >
         <h1
@@ -24,7 +30,30 @@ function App() {
         </h6>
         <div className='card-body'>
           <h5 className='card text-white bg-dark mb-3'>Add Your Task</h5>
+          <span className='card-text'>
+            <input
+              type='text'
+              className='mb-2 form-control titleIn'
+              placeholder='Title'
+            />
+            <input
+              type='text'
+              className='mb-2 form-control desIn'
+              placeholder='Description'
+            />
+            <button
+              className='btn btn-outline-primary mx-2 mb-4'
+              style={{ borderRadius: '50px', 'font-weight': 'bold' }}
+            >
+              Add Task
+            </button>
+          </span>
+          <h5 className='card text-white bg-dark mb-3'>List of Tasks</h5>
+          <div>{/* Todo items component */}</div>
         </div>
+        <h6 className='card text-dark bg-warning py-1 mb-0'>
+          Copyright 2024, All right reserved &copy;
+        </h6>
       </div>
     </div>
   );
